b154bcfe2c74a4528963571dfc7aff0a511397dd
Edwin van den Belt <edwinvandenbelt@hotmail.com>
2022-09-23 16:51:49 +0200

    removed old css, changed tailwind.config (colors)

--- tools/tool-use-case-shop/tailwind.config.js
+++ tools/tool-use-case-shop/tailwind.config.js
@@ -5,7 +5,60 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
   ],
   "theme": {
-    "extend": [],
+    "extend": {
+      "colors": {
+        "blue": {
+          "50": "#E6E8FC",
+          "200": "#B3BAF6",
+          "400": "#7993FA",
+          "500": "#3159FD",
+          "600": "#0032FF",
+          "700": "#1d4ed8",
+          "900": "#001A73"
+        },
+        "yellow": {
+          "50": "#E0F97B",
+          "100": "#DCFB62",
+          "200": "#CDFF00",
+          "500": "#e0f97b",
+          "600": "#DCFB62",
+          "700": "#CDFF00"
+        },
+        "gray": {
+          "100": "#EFEFF0",
+          "200": "#D4D4D4",
+          "300": "#D1D5DB",
+          "500": "#6B7280",
+          "900": "#4B5563"
+        },
+        "green": {
+          "500": "#5DC001",
+          "700": "#0D8938"
+        },
+        "red": {
+          "200": "#FECACA",
+          "500": "#FE005B"
+        },
+        "amber": {
+          "400": "#FFC801",
+          "500": "#FF7101"
+        },
+        "neutral": {
+          "200": "#E5E5E5",
+          "300": "#D4D4D4",
+          "400": "#A1A1AA"
+        },
+        "white": {
+          "DEFAULT": "#FFFFFF"
+        },
+        "black": {
+          "DEFAULT": "#000000"
+        },
+        "transparent": "transparent",
+        "current": "currentColor",
+        "inherit": "inherit"
+      }
+    },
     "fontFamily": {
       "sans": [
         "\"Rajdhani\"",
@@ -62,58 +115,6 @@ module.exports = {
         "72px",
         "80px"
       ]
-    },
-    "colors": {
-      "blue": {
-        "50": "#E6E8FC",
-        "200": "#B3BAF6",
-        "400": "#7993FA",
-        "500": "#3159FD",
-        "600": "#0032FF",
-        "700": "#1d4ed8",
-        "900": "#001A73"
-      },
-      "yellow": {
-        "50": "#E0F97B",
-        "100": "#DCFB62",
-        "200": "#CDFF00",
-        "500": "#E0F97B",
-        "600": "#DCFB62",
-        "700": "#CDFF00"
-      },
-      "gray": {
-        "100": "#EFEFF0",
-        "200": "#D4D4D4",
-        "300": "#D1D5DB",
-        "500": "#6B7280",
-        "900": "#4B5563"
-      },
-      "green": {
-        "500": "#5DC001",
-        "700": "#0D8938"
-      },
-      "red": {
-        "200": "#FECACA",
-        "500": "#FE005B"
-      },
-      "amber": {
-        "400": "#FFC801",
-        "500": "#FF7101"
-      },
-      "neutral": {
-        "200": "#E5E5E5",
-        "300": "#D4D4D4",
-        "400": "#A1A1AA"
-      },
-      "white": {
-        "DEFAULT": "#FFFFFF"
-      },
-      "black": {
-        "DEFAULT": "#000000"
-      },
-      "transparent": "transparent",
-      "current": "currentColor",
-      "inherit": "inherit"
     }
   },
   plugins: [
